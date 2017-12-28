be99122106f5b8caf7c4419e072521fbffaede03
mpaso21 <fabcomlist@gmail.com>
2017-12-28 14:42:08 +0100

    menu

---
+++ src/application/FXMLMenuController.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+
+public class FXMLMenuController {
+@FXML private Text actiontarget;
+
+    
+	
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("hai schiacciato play");
+    }
+}
